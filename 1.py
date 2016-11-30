084e70c315dc8d3d63c9fc33e860e0ad7e7429f2
Gyorgy Gabor <gyorgyg@gmail.com>
2016-11-30 18:00:47 +0100

    Trial-exam-python exercise 1.py is done'

--- 1.py
+++ 1.py
@@ -1,3 +1,14 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
+
+def double_element(element_list):
+    try:
+        for i in range(len(element_list)):
+            element_list[i] *= 2
+        return element_list
+    except TypeError:
+        return 'It isn\'t a list!'
+
+print(double_element([1,2,3,4]))
+print(double_element('x'))
